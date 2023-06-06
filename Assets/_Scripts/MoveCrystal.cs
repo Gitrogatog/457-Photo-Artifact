0cc8a2fb013925211d755e0a27de5649116dfbf0
ary <aryan@jha.io>
2023-06-05 22:14:30 -0700

    final touches

--- Assets/_Scripts/MoveCrystal.cs
+++ Assets/_Scripts/MoveCrystal.cs
@@ -7,7 +7,7 @@ public class MoveCrystal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0.0f, 0.0f, Time.deltaTime * 10.0f);
-        transform.Translate(0.01f * Vector3.forward * Mathf.Sin(Time.realtimeSinceStartup));
+        transform.Rotate(0.0f, 0.0f, Time.deltaTime * 20.0f);
+        transform.Translate(0.001f * Vector3.forward * Mathf.Sin(Time.realtimeSinceStartup));
     }
 }
